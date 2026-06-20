0e24fd08b9ce0851a001919f4bed2bc43adee7d5
Veeresh-Abbili <abbiliveeresh98@gmail.com>
2026-06-20 17:33:04 +0530

    *ngIf vs @if and *ngFor vs @for implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { DataBindingComponent } from './data-binding/data-binding.component';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { RouterOutlet } from '@angular/router';
+import { StructuralDirectiveNgifVsIfComponent } from './components/Directives/structural-directive-ngif-vs-if/structural-directive-ngif-vs-if.component';
+import { StructuralDirectiveNgforVsForComponent } from './components/Directives/structural-directive-ngfor-vs-for/structural-directive-ngfor-vs-for.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,DataBindingComponent],
+  imports: [RouterOutlet,
+    // StructuralDirectiveNgifVsIfComponent,
+    StructuralDirectiveNgforVsForComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
